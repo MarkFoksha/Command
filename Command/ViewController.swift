a721d237ab7be4b34a34d52d9f1bfee61021e13c
Mark Foksha <mark.foksha1813@gmail.com>
2023-07-12 00:45:01 +0300

    Initial Commit

---
+++ Command/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Command
+//
+//  Created by Марк Фокша on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
